8ab9080a299b5e324e1896aee271a06a0974aa3e
Brendan Buslig <ffbrendan@free.fr>
2021-09-06 22:41:55 +0200

    fetch index

--- Front-end/js/index.js
+++ Front-end/js/index.js
@@ -1,81 +1,19 @@
-// ------------------------- Récupération des données de l'API ----------------------- //
-/*
-main()
+// ------Récupération des données de l'API + Affichage des éléments à récupérer -------------- //
 
-async function main() {
-    const articles = await getArticles()
-
-    for (article of articles) {
-        displayArticle(article)
-    }
-}
-*/
-  
- 
-// -------------------------- Appel des données de l'API ------------------------ //
-/*
-function getArticles() {
-    return fetch("http://localhost:3000/api/teddies")
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (articles) {
-            return articles
-        })
-        .catch(function (error) {
-            alert(error);
-        })
-}
-*/
-
-let teddies = function () {
-
-    fetch("http://localhost:3000/api/teddies")
-        .then(function (response) {
-            return response.json();
-        }).catch(function (error) {
-            console.log("Erreur : " + error);
-        })
-    document.querySelector("#id").innerHTML = id;
-    document.querySelector("#name").innerHTML = name;
-    document.querySelector("#price").innerHTML = price;
-    document.querySelector("#description").innerHTML = description;
-    document.querySelector("#imageUrl").innerHTML = imageUrl;
-}
-
-// -------------------------- Affichage les éléments récupérer ----------------------- //
-
-// 1 ere méthode(un peu brouillon) :
-/*
-function displayArticle() {
-    document.getElementById("main").innerHTML += `
-    <article class="blog">
-        <h2 id="blog__title" class="blog__title">${article.title}</h2>
-        <p id="blog__body" class="blog__body">${article.body}</p>
-    </article>`
-}
-*/
-
-/*
-// 2 eme méthode :
-
-function displayArticle(article) {
-    const templateElement = document.getElementById("templateArticle")
-    const cloneElement = document.importNode(templateElement.content, true);
-
-    cloneElement.getElementById("blog__title").textContent = article.name
-    cloneElement.getElementById("blog__body").textContent = article.price
-    cloneElement.getElementById("blog__body1").textContent = article.description
-    cloneElement.getElementById("blog__body2").textContent = article.colors
-    cloneElement.getElementById("blog__link").href += "?id=" + article.id
-
-    document.getElementById("main").appendChild(cloneElement)
-}
-*/
-
-document.querySelector("ours").addEventListener("submit", function (e) {
-    e.preventDefault();
-    let ours = document.querySelector("#main").value;
-
-    teddies(ours);
-});
+fetch("http://localhost:3000/api/teddies")
+    .then(function (response) {
+        return response.json();
+    }).then(function (produits) {
+        for (let produit of produits) {
+            document.getElementById("main").innerHTML += `
+                <div class="produits">
+                    <a href="produit.html?id=${produit._id}" id="produit" class="produit">
+                    <img id="teddies-img" class="teddies-img" src="${produit.imageUrl}" alt="teddies">
+                    <h2 id="teddies-name" class="teddies-name">${produit.name}</h2>
+                    <p p id="teddies-descritpion" class="teddies-descritpion">${produit.description}</p>
+                    <p id="teddies-prix" class="teddies-prix">${produit.price * 0.01}€</p>
+                </div>`
+        }
+    }).catch(function (error) {
+        console.log("Erreur : " + error);
+    })
