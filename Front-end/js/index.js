b4ace8ae68a53c8fd7d2c8b0a2c4899a37f39507
Brendan Buslig <ffbrendan@free.fr>
2021-09-08 16:42:20 +0200

    css index + html produit et commande

--- Front-end/js/index.js
+++ Front-end/js/index.js
@@ -7,11 +7,13 @@ fetch("http://localhost:3000/api/teddies")
         for (let produit of produits) {
             document.getElementById("main").innerHTML += `
                 <div class="produits">
-                    <a href="produit.html?id=${produit._id}" id="produit" class="produit">
-                    <img id="teddies-img" class="teddies-img" src="${produit.imageUrl}" alt="teddies">
-                    <h2 id="teddies-name" class="teddies-name">${produit.name}</h2>
-                    <p p id="teddies-descritpion" class="teddies-descritpion">${produit.description}</p>
-                    <p id="teddies-prix" class="teddies-prix">${produit.price * 0.01}€</p>
+                <a id="teddies-lien" class="teddies-lien" href="html/produit.html?id=${produit._id}">
+                    <img id="teddies-img" class="teddies-img" src="${produit.imageUrl}" alt="Teddies">                    
+                    <div id="teddies-name-description-prix" class="teddies-name-description-prix">
+                        <h2 id="teddies-name" class="teddies-name">${produit.name}</h2>
+                        <p id="teddies-descritpion" class="teddies-descritpion">${produit.description}</p>                    
+                        <p id="teddies-prix" class="teddies-prix">${produit.price * 0.01} €</p>                                                
+                    </div>
                 </div>`
         }
     }).catch(function (error) {
