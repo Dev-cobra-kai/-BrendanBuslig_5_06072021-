8b642958b0d999a67455b5a7b2bd48b0e3954916
Brendan Buslig <ffbrendan@free.fr>
2021-09-26 19:18:51 +0200

    css + js + html produit

--- Front-end/js/produit.js
+++ Front-end/js/produit.js
@@ -1,105 +1,109 @@
 
-
-
 const produitId = getProduitId()
 
 function getProduitId() {
     return new URL(location.href).searchParams.get("id")
 }
 
+console.log(produitId);
+
 fetch(`http://localhost:3000/api/teddies/${produitId}`)
     .then(function (response) {
         return response.json();
     }).then(function (produit) {
         displayProduit(produit)
+
     }).catch(function (error) {
         console.log("Erreur : " + error);
     })
 
+// Affichage du Teddy
+
 function displayProduit(produit) {
-    const colors = displayColors(produit)
 
-    document.getElementById("main").innerHTML += `
-                <div class="produit">
-                    <img id="teddy-img" class="teddy-img" src="${produit.imageUrl}" alt="Teddy">                    
-                    <div id="teddy-name-description-prix" class="teddy-name-description-prix">
-                        <h2 id="teddy-name" class="teddy-name">${produit.name}</h2>
-                        <p id="teddy-descritpion" class="teddy-descritpion">${produit.description}</p>  
-                        <div id="teddy-couleurs-quantité-prix" class="teddy-couleurs-quantité-prix">
-                            <div id="teddy-couleurs-quantité" class="teddy-couleurs-quantité">
-                                <div id="teddy-couleurs" class="teddy-couleurs">
-                                    <label for="select-color">Choisir une couleur</label>
-                                    <select class="selection-couleur" id="selection-couleur">
-                                    ${colors}
-                                    </select>                         
-                                </div>
-                                <div id="teddy-quantite" class="teddy-quantite">
-                                    <label for="select-quantity">Choisir une quantité</label>
-                                    <input type="number" class="selection-quantite" id="selection-quantite" size="5" value="1" required>
-                                </div>
-                            </div>
-                            <p id="teddy-prix" class="teddy-prix">${produit.price * 0.01} €</p> 
-                            <input type="submit" class="btn btn-primary btn-panier" id=" btn btn-panier" value="Ajouter au panier">
-                        </div>                                      
-                    </div>
-                </div>`
-   
-}
+    document.getElementById("teddy-img").src = produit.imageUrl
+    document.getElementById("teddy-name").innerText = produit.name
+    document.getElementById("teddy-description").innerText = produit.description
+    document.getElementById("selection-couleur").innerText = produit.colors
+    // document.getElementById("selection-quantite").innerText = quantity
+    document.getElementById("teddy-prix").innerText = `${produit.price * 0.01} €`
 
-// Affichage du choix des couleurs
-function displayColors(produit) {
-
-    let selectColor = ""
+    // Affichage du choix des couleurs    
+    let selectColor = document.getElementById("selection-couleur");
     for (let i = 0; i < produit.colors.length; i++) {
-        selectColor += `<option value ="${produit.colors[i]}">${produit.colors[i]}</option>`
+        let choiceColor = document.createElement("option");
+        choiceColor.innerText = produit.colors[i];
+        selectColor.appendChild(choiceColor);
     }
-    return selectColor
-}
-/*
-// Affichage de la quantité
-function displayQuantity(produit) {
-    let selectQuantity = ""
-    for (let i = 0; i < produit.quantities.length; i++) {
-        selectQuantity += `<option value =${produit.quantities[i]}">${produit.quantities[i]}</option>`;
-    }
-    return selectQuantity
-}
-*/
 
-/******************* Le Panier ************************/
+    // Affichage de la quantité
+    // const selectQuantity = document.getElementById("selection-quantite");
+    // for (let i = 0; i < produit.quantities.length; i++) {
+    //     let choiceQuantity = document.createElement("option");
+    //     choiceQuantity.innerText = produit.quantities[i];
+    //     selectQuantity.appendChild(choiceQuantity);
+    // }
+
+}
+
+/************************************ LE PANIER ************************************/
 
 // Récupération des données sélectionnées par l'utilisateur = quantité + couleur
 
+const btn_panier = document.getElementById("btn-panier")
 
-function envoyerPanier(produit) {
+btn_panier.addEventListener("click", function (e) {
+    e.preventDefault();
 
-    const btn_panier = document.getElementById("btn-panier")
-
-    btn_panier.addEventListener("click", function (e) {
-        e.preventDefault();
-       
-        let selectColor = document.getElementById("selection-couleur").value;
-        let selectQuantite = parseInt(document.getElementById("selection-quantite").value);
-
+    function envoyerPanier(produit) {
         let objet = {
-            id: produit._id,
+            id: produitId._id,
             nom: produit.name,
-            prix: produit.price,
             couleur: selectColor,
             quantite: selectQuantite,
+            prix: produit.price,
+        };
+        console.log(envoyerPanier);
+    };
+    alert("Article ajouté au panier !!!")
+    
+})
+
+/*
+
+    function envoyerPanier (produit) {
+
+        let idTeddy = produitId
+        let nameTeddy = document.getElementById("teddy-name").value;
+        let selectColor = document.getElementById("selection-couleur").value;
+        let selectQuantite = parseInt(document.getElementById("selection-quantite").value);
+        let priceTeddy = document.getElementById("teddy-prix").value;
+
+        let objet = {
+            id: produitId,
+            nom: nameTeddy,
+            couleur: selectColor,
+            quantite: selectQuantite,
+            prix: priceTeddy,
         };
 
-        let monPanier = JSON.parse(localStorage.getItem("monPanier"));
-        monPanier.push(objet)      
-        const onlinePanier = JSON.stringify(monPanier)
-        localStorage.setItem("monPanier", onlinePanier)
+    };
 
-        alert("Article ajouté au panier !!!")
-    })    
-}
-
-
-/******************* Local Storage *********************/
+    console.log(envoyerPanier);
+*/
+/************************************* LOCAL STORAGE *********************************/
 
 // JSON.parse => convertit les données JSON en objet Javascript
 // JSON.stringify => convertit en JSON
+
+/*
+
+    let monPanier = JSON.parse(localStorage.getItem("monPanier"));
+    monPanier.push(objet)
+    const onlinePanier = JSON.stringify(monPanier)
+    localStorage.setItem("monPanier", onlinePanier)
+
+    alert("Article ajouté au panier !!!")
+
+})
+*/
